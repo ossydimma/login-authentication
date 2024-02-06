804e2aaf451d3069c80fafc7592e202b67f96425
Osita <chrisjerry070@gmail.com>
2024-02-07 01:22:25 +0400

    adding functionality

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ const signUpBtn = document.querySelector(".Sign-btn");
 const registerBtn = document.querySelector(".register-btn");
 const username = document.querySelector(".username");
 const password = document.querySelector(".password");
+const logUsername = document.querySelector(".log-username");
+const logPassword = document.querySelector(".log-password");
 const confirmPassword = document.querySelector(".confirm-password");
 const feedBack = document.querySelector(".feedBack");
+const access = document.querySelector(".access");
 
-const storedData = JSON.parse(localStorage.getItem('userInfo') || {}) 
+const storedData = JSON.parse(localStorage.getItem('userInfo') ) || undefined;
 
 const data = {
   userName: "",
@@ -48,15 +51,32 @@ registerBtn.addEventListener("click", (event) => {
         data.userName = username.value;
         data.password = password.value;
         localStorage.setItem('userInfo', JSON.stringify(data))
-        storedData.length === 0 ? alert('hello') : console.log(storedData);
+        // storedData.length === 0 ? alert('hello') : console.log(storedData);
         console.log(`name : ${data.userName}, pass : ${data.password}`);
       } else {
-        feedBack.style.display = "block";
         feedBack.innerText = `both passwords does not match`;
       }
     } else {
-      feedBack.style.display = "block";
+      feedBack.style.display = "form must be filled";
     }
     registerBtn.innerHTML = `Register`;
   }, 2000);
 });
+
+access.addEventListener('click', (e)=> {
+  e.preventDefault()
+
+  if (logUsername.value.length !== 0  &&  logPassword.value.length !== 0) {
+    if (storedData !== undefined) {
+      if (storedData.userName === logUsername.value && storedData.password === logPassword.value ) {
+        alert('logged in')
+      } else {
+        feedBack.innerText = `You entered Incorrect data`
+      }
+    } else {
+      feedBack.innerText = `Account does not exist`
+    }
+  } else {
+    feedBack.innerText = `form must be fill`
+  }
+})
